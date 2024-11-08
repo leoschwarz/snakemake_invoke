9d08094b43ea8d01ec24385c147c7087a261b062
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-11-08 16:08:13 +0100

    simplify version

--- tests/test_package.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-import importlib.metadata
-
-import snakemake_invoke as m
-
-
-def test_version():
-    assert importlib.metadata.version("snakemake_invoke") == m.__version__
