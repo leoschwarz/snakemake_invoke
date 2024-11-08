236d401e5d0c7ef7b340b8719875a07a1e608f5f
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-11-08 15:55:38 +0100

    add tests

---
+++ tests/invoke/test_invoke_call_function.py
@@ -0,0 +1,38 @@
+import os
+import pytest
+from snakemake_invoke.config import SnakemakeInvokeConfig
+from snakemake_invoke.invoke.invoke_call_function import InvokeCallFunction
+
+
+@pytest.fixture(params=[None])
+def config_env_variables(request) -> dict[str, str]:
+    return request.param or {}
+
+
+@pytest.fixture
+def config(config_env_variables):
+    return SnakemakeInvokeConfig(
+        env_variables=config_env_variables,
+    )
+
+
+@pytest.fixture()
+def invoke(config):
+    return InvokeCallFunction(config=config)
+
+
+@pytest.mark.parametrize(
+    "config_env_variables,expected",
+    [
+        ({}, {"a": "1", "b": "2"}),
+        ({"c": "3"}, {"a": "1", "b": "2", "c": "3"}),
+        ({"a": "x", "c": "3"}, {"a": "x", "b": "2", "c": "3"}),
+    ],
+    indirect=["config_env_variables"],
+)
+def test_set_env_vars(mocker, invoke, expected):
+    original_env_vars = {"a": "1", "b": "2"}
+    mocker.patch("os.environ", original_env_vars)
+    with invoke._set_env_vars():
+        assert dict(os.environ) == expected
+    assert dict(os.environ) == {"a": "1", "b": "2"}
---
+++ tests/invoke/test_invoke_subprocess.py
@@ -0,0 +1,56 @@
+import pytest
+import sys
+from pathlib import Path
+from snakemake_invoke.config import SnakemakeInvokeConfig
+from snakemake_invoke.invoke.invoke_subprocess import InvokeSubprocess
+
+
+@pytest.fixture()
+def config_snakefile_path() -> Path:
+    return Path("/tmp/workflow/Snakefile")
+
+
+@pytest.fixture
+def config(config_snakefile_path):
+    return SnakemakeInvokeConfig(snakefile_path=config_snakefile_path)
+
+
+@pytest.fixture
+def invoke(config):
+    return InvokeSubprocess(config=config)
+
+
+@pytest.mark.parametrize(
+    "input_args,expected",
+    [
+        pytest.param(["a"], "a", id="plain_string"),
+        pytest.param(["a b"], "'a b'", id="string_with_space"),
+        pytest.param(['"a b"'], "'\"a b\"'", id="string_with_double_quotes"),
+        pytest.param(["a'b"], "'a'\"'\"'b'", id="string_with_single_quote"),
+        pytest.param(['a"b'], "'a\"b'", id="string_with_double_quote"),
+        pytest.param(["a", "a b", ""], "a 'a b' ''", id="list_of_strings"),
+    ],
+)
+def test_args_to_shell_command(input_args, expected):
+    result = InvokeSubprocess._args_to_shell_command(input_args)
+    assert result == expected
+
+
+def test_get_base_command(invoke) -> None:
+    base_command = invoke.get_base_command(
+        extra_args=["--keep-going"],
+        work_dir=Path("/tmp/work"),
+    )
+    assert base_command == [
+        sys.executable,
+        "-m",
+        "snakemake",
+        "-d",
+        "/tmp/work",
+        "--cores",
+        "1",
+        "--snakefile",
+        "/tmp/workflow/Snakefile",
+        "--rerun-incomplete",
+        "--keep-going",
+    ]
